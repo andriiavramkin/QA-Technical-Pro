282b97999301bb590fdcbf88016f955b4797f4be
Andrii Avramkin <avramkin.ad@gmail.com>
2024-02-07 15:00:18 +0100

    hw-3

---
+++ homework/hw3.js
@@ -0,0 +1,93 @@
+// const result = 'number' + 3 + 3
+// //the result variable is String now. And adding 3 and 3 will simple add the numbers to our string value.
+// console.log(result)   // returns: number33
+
+
+// const result = null + 3
+// //
+// console.log(result) // returns: 3
+
+
+// const result = 5 && "qwerty"
+// //
+// console.log(result) // returns: qwerty
+
+
+// const result = +'40' + +'2' + "hillel";
+// //
+// console.log(result) // returns: 42hillel
+
+
+// const result = '10' - 5 === 6;
+// //
+// console.log(result) // returns: false
+
+
+// const result = true + false
+// //
+// console.log(result) // returns: 1
+
+
+// const result = '4px' - 3
+// //
+// console.log(result) //returns: NaN
+
+
+// const result = '4' - 3
+// //
+// console.log(result) //returns:  1
+
+
+// const result = '6' + 3 ** 0;
+// //
+// console.log(result) //returns: 61
+
+
+// const result = 12 / '6'
+// //
+// console.log(result) //returns:  2
+
+
+// const result = '10' + (5 === 6);
+// //
+// console.log(result) //returns:  10false
+
+
+// const result = null == ''
+// //
+// console.log(result) //returns: false
+
+
+// const result = 3 ** (9 / 3);
+// //
+// console.log(result) //returns:  27
+
+
+// const result = !!'false' == !!'true'
+// //  !! converts String to boolean meaning that we compare (true == true)
+// console.log(result) //returns:  true
+
+
+// const result = 0 || '0' && 1
+// //
+// console.log(result) //returns: 1
+
+
+// const result = (+null == false) < 1;
+// //
+// console.log(result) //returns:  false
+
+
+// const result = false && true || true
+// //
+// console.log(result) //returns:  true
+
+
+// const result = false && (false || true);
+// //
+// console.log(result) //returns:  false
+
+
+// const result = (+null == false) < 1 ** 5;
+// //
+// console.log(result) //returns: false
